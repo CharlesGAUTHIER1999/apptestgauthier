36b0e47219bf235f19443546dcc1d75035f51786
charles gauthier <charles.gauthier99@gmail.com>
2025-09-19 15:18:09 +0200

    modifs

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,6 @@ use App\Http\Controllers\PostController;
 Route::get('/', [PageController::class, 'home'])->name('home');
 Route::get('/hello', static fn () => 'hello laravel')->name('hello');
 Route::get('/hello-view', static fn () => view('home', ['title' => 'Hello view']))->name('hello.view');
-Route::resource('posts', PostController::class)->only(['index', 'show']);
-Route::middleware('admin')->group(function() {
-    Route::resource('posts', PostController::class)
-        ->only(['create', 'store', 'edit', 'update', 'destroy']);
-});
+Route::resource('posts', PostController::class)
+    ->only(['index', 'show', 'create', 'store', 'edit', 'update', 'destroy']);
+
